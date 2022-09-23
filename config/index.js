8543f0b35dd48e40526b471646519f08c222565a
Lycan <42599655+1603729255@users.noreply.github.com>
2022-09-23 16:49:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -81,6 +81,7 @@ export const USER_CONFIG = {
         {"type": "*生日", "name": "老婆", "year": "1994", "date": "08-23"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {"type": "*生日", "name": "老公", "year": "1995", "date": "09-07"},
+        {"type": "节日", "name":"结婚纪念日","year": "2021", "date": "03-14"}
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
